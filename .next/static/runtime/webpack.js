d91654c6d5a730053725d492546fb61cc50e4420
Eugene Birondo <jbirondo@gmail.com>
2020-03-23 22:53:00 -0700

    Added next/link to index.js

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "26bf7eeed2346357706d";
+/******/ 	var hotCurrentHash = "ab0f761ef30eac1b859d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
