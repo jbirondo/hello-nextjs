ddcb063af68ebed71c484caa335fd1b54811e98b
Eugene Birondo <jbirondo@gmail.com>
2020-03-24 23:23:44 -0700

    Deleted Link to about in index.js

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "80c3dd5729fa5a8b5f16";
+/******/ 	var hotCurrentHash = "6ab9ecd6aa3fc1063040";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
