7a41a600c786a2048d76712b9b600d263764f7c4
Eugene Birondo <jbirondo@gmail.com>
2020-03-27 22:51:50 -0700

    Fetching Data for Pages exercise

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "4b9aa12ad46cd836920c";
+/******/ 	var hotCurrentHash = "ed1c6c3b1aa6d8801264";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
