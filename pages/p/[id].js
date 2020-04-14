7cfd2e07f1043839c364a140308f424cf53a122b
Eugene Birondo <jbirondo@gmail.com>
2020-04-13 23:03:10 -0700

    Fetching Data for Pages

--- pages/p/[id].js
+++ pages/p/[id].js
@@ -1,15 +1,15 @@
-import Layout from "../../components/MyLayout";
-import fetch from "isomorphic-unfetch";
+import Layout from '../../components/MyLayout';
+import fetch from 'isomorphic-unfetch';
 
 const Post = props => (
   <Layout>
     <h1>{props.show.name}</h1>
-    <p>{props.show.summary.replace(/<[/]?[pb]>/g, "")}</p>
+    <p>{props.show.summary.replace(/<[/]?[pb]>/g, '')}</p>
     {props.show.image ? <img src={props.show.image.medium} /> : null}
   </Layout>
 );
 
-Post.getInitialProps = async function(context) {
+Post.getInitialProps = async function (context) {
   const { id } = context.query;
   const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
   const show = await res.json();
